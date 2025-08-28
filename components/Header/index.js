84c4084ad4773e0e42019cbaceb26511830e57d6
Junwon Kim <junwonkim04@outlook.com>
2025-08-28 16:37:35 +0900

    Change email adress

--- components/Header/index.js
+++ components/Header/index.js
@@ -86,7 +86,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:junwonkim59@gmail.com")}
                   >
                     Contact
                   </Button>
@@ -109,7 +109,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
                   )}
 
                   <Button
-                    onClick={() => window.open("mailto:hello@chetanverma.com")}
+                    onClick={() => window.open("mailto:junwonkim59@gmail.com")}
                   >
                     Contact
                   </Button>
@@ -146,7 +146,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:junwonkim59@gmail.com")}>
               Contact
             </Button>
             {mounted && theme && data.darkMode && (
@@ -177,7 +177,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
               </Button>
             )}
 
-            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>
+            <Button onClick={() => window.open("mailto:junwonkim59@gmail.com")}>
               Contact
             </Button>
 
